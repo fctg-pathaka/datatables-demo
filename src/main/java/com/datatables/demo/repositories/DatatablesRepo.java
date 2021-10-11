6eaef6d9142691cbd31e1afdf31b55b7cb92667e
vns-ankit <pathak.sn.anurag@gmail.com>
2021-10-11 14:35:32 +0530

    revert
    revert

--- src/main/java/com/datatables/demo/repositories/DatatablesRepo.java
+++
@@ -1,7 +0,0 @@
-package com.datatables.demo.repositories;
-
-import com.datatables.demo.beans.Datatables;
-import org.springframework.data.jpa.datatables.repository.DataTablesRepository;
-
-public interface DatatablesRepo extends DataTablesRepository<Datatables, Long> {
-}
--- src/main/webapp/WEB-INF/jsp/shared/footer.jsp
+++
@@ -1,4 +0,0 @@
-<footer class="footer">
-    <div class="footer-content text-center py-3"> &#169; 2021,
-    </div>
-</footer>
